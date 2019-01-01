5b6ec43bb77467d30e446a83255c0e00765b774a
Miha Tratnik <mihakaucic@gmail.com>
2019-01-01 21:31:50 +0100

    changes to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_url
@@ -8,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '876d7d641614db92287481d8056d7bcf'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 flask_bcrypt = Bcrypt(app)
@@ -16,6 +17,7 @@ login_manager = LoginManager(app)
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = "info"
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 from app import routes, models
