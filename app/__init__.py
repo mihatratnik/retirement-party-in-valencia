fe5a4bb5a28334a860c38b12cb3b3233cb4e06d0
Miha Tratnik <mihakaucic@gmail.com>
2019-01-03 11:29:50 +0100

    migrating to heroku postgres

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,11 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+DATABASE_URL = os.environ['DATABASE_URL']
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '876d7d641614db92287481d8056d7bcf'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = psycopg2.connect(DATABASE_URL, sslmode='require')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 flask_bcrypt = Bcrypt(app)
@@ -17,7 +18,7 @@ login_manager = LoginManager(app)
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = "info"
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 from app import routes, models
