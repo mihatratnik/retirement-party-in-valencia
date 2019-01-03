7b79bd9f4fc9655f5323b6c968d231ad8aa80d80
Miha Tratnik <mihakaucic@gmail.com>
2019-01-03 11:09:38 +0100

    back to sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '876d7d641614db92287481d8056d7bcf'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 flask_bcrypt = Bcrypt(app)
@@ -17,7 +17,7 @@ login_manager = LoginManager(app)
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = "info"
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 from app import routes, models
